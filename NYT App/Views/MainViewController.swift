868e1ae7f1ca2e120a6178cf08faa7fb28fdf11f
nour_araar <nour_araar_2009@yahoo.com>
2022-05-21 22:41:59 +0800

    Create MainViewController and write some tests

--- NYT App/Views/MainViewController.swift
+++ NYT App/Views/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  NYT App
 //
 //  Created by Nour Araar on 5/21/22.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UITableViewController {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
---
+++ NYT AppTests/Views/MainViewControllerTests.swift
@@ -0,0 +1,38 @@
+//
+//  MainViewControllerTests.swift
+//  NYT AppTests
+//
+//  Created by Nour Araar on 5/21/22.
+//
+
+import XCTest
+@testable import NYT_App
+
+class MainViewControllerTests: XCTestCase {
+    
+    var sut: MainViewController!
+
+    override func setUpWithError() throws {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        sut = try XCTUnwrap(storyboard.instantiateViewController(withIdentifier: "MainViewController") as? MainViewController)
+        sut.loadViewIfNeeded()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    
+    func test_shouldHaveTableView() {
+        XCTAssertTrue(sut.tableView.isDescendant(of: sut.view))
+    }
+    
+    
+    func test_tableViewShouldHaveTwoSections() {
+        XCTAssertEqual(sut.tableView.numberOfSections, 2)
+    }
+    
+    
+
+}
